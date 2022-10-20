d44841c58525aa30c8bd40e1a04a42beb37d43b2
Roarco <sebuscaroberth@gmail.com>
2022-10-19 20:33:03 -0500

    Recibiendo paramentros en las peticiones get

--- src/app.controller.ts
+++ src/app.controller.ts
@@ -1,4 +1,4 @@
-import { Controller, Get } from '@nestjs/common';
+import { Controller, Get, Param } from '@nestjs/common';
 import { AppService } from './app.service';
 
 @Controller()
@@ -13,5 +13,18 @@ export class AppController {
   @Get('nuevo')
   getNuevo(): string {
     return 'Nuevo';
+  }
+
+  @Get('products/:productId')
+  getProduct(@Param('productId') productId: string) {
+    return `Product ${productId}`;
+  }
+
+  @Get('categories/:categoryId/products/:productId')
+  getCategory(
+    @Param('categoryId') categoryId: string,
+    @Param('productId') productId: string,
+  ) {
+    return `Category ${categoryId} Product ${productId}`;
   }
 }
