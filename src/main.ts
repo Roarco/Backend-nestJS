1aaa8d2046959fc3824be378e12baac1cd7c0acd
Roarco <sebuscaroberth@gmail.com>
2022-10-20 19:23:59 -0500

    evitando parametros incorrectos

--- src/main.ts
+++ src/main.ts
@@ -4,7 +4,12 @@ import { AppModule } from './app.module';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
-  app.useGlobalPipes(new ValidationPipe());
+  app.useGlobalPipes(
+    new ValidationPipe({
+      whitelist: true, // <-- remove all properties that are not defined in the DTO
+      forbidNonWhitelisted: true, // <-- throw an error if a property is not defined in the DTO
+    }),
+  );
   await app.listen(3000);
 }
 bootstrap();
