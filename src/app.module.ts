f9796e2b2c41d6bc3c5be939770f2957130769d9
Roarco <sebuscaroberth@gmail.com>
2022-10-22 14:53:41 -0500

    dandole uso a useFactory para llamados asincronos

--- src/app.module.ts
+++ src/app.module.ts
@@ -4,11 +4,14 @@ import { AppService } from './app.service';
 import { UsersModule } from './users/users.module';
 import { ProductsModule } from './products/products.module';
 
+import { HttpModule } from '@nestjs/axios';
+//import { firstValueFrom } from 'rxjs';
+
 const API_key = '54564654';
 const API_key_PROD = 'Prod54564654';
 
 @Module({
-  imports: [UsersModule, ProductsModule],
+  imports: [HttpModule, UsersModule, ProductsModule],
   controllers: [AppController],
   providers: [
     AppService,
@@ -16,6 +19,17 @@ const API_key_PROD = 'Prod54564654';
       provide: 'API_KEY',
       useValue: process.env.NODE_ENV === 'production' ? API_key_PROD : API_key,
     },
+    // {
+    //   provide: 'TASKS',
+    //   useFactory: async (http: HttpService) => {
+    //     const tasks = await http.get(
+    //       'https://jsonplaceholder.typicode.com/todos',
+    //     );
+    //     const data = await (await firstValueFrom(tasks)).data;
+    //     return data;
+    //   },
+    //   inject: [HttpService],
+    // },
   ],
 })
 export class AppModule {}
--- src/app.service.ts
+++ src/app.service.ts
@@ -1,9 +1,23 @@
 import { Injectable, Inject } from '@nestjs/common';
 
+import { HttpService } from '@nestjs/axios';
+import { firstValueFrom } from 'rxjs';
 @Injectable()
 export class AppService {
-  constructor(@Inject('API_KEY') private apiKey: string) {}
+  constructor(
+    @Inject('API_KEY') private apiKey: string,
+    //@Inject('TASKS') private tasks: any[],
+    readonly http: HttpService,
+  ) {}
   getHello(): string {
     return `Hello World! ${this.apiKey}`;
+  }
+
+  async getTasks() {
+    const tasks = await this.http.get(
+      'https://jsonplaceholder.typicode.com/todos',
+    );
+    const data = await (await firstValueFrom(tasks)).data;
+    return data;
   }
 }
