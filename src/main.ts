9ac5762b702afe2d83e832690abc450f7bf9e82b
Roarco <sebuscaroberth@gmail.com>
2022-10-23 23:33:16 -0500

    Habilitando cors

--- src/main.ts
+++ src/main.ts
@@ -6,6 +6,7 @@ import { AppModule } from './app.module';
 async function bootstrap() {
   const PORT = process.env.PORT || 3000;
   const app = await NestFactory.create(AppModule);
+  app.enableCors();
 
   app.useGlobalPipes(
     new ValidationPipe({
