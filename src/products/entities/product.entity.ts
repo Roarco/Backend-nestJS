d21ddc48e535854f6f80fc0e1c5e2656ec2a4899
Roarco <sebuscaroberth@gmail.com>
2022-11-02 13:33:55 -0500

    Modificando una entidad con migraciones

--- src/products/entities/product.entity.ts
+++ src/products/entities/product.entity.ts
@@ -1,5 +1,11 @@
 import { ApiProperty } from '@nestjs/swagger';
-import { PrimaryGeneratedColumn, Column, Entity } from 'typeorm';
+import {
+  PrimaryGeneratedColumn,
+  Column,
+  Entity,
+  CreateDateColumn,
+  UpdateDateColumn,
+} from 'typeorm';
 @Entity()
 export class Product {
   @ApiProperty()
@@ -25,4 +31,16 @@ export class Product {
   @ApiProperty()
   @Column({ type: 'varchar', length: 255, nullable: false })
   image: string;
+
+  @CreateDateColumn({
+    type: 'timestamptz',
+    default: () => 'CURRENT_TIMESTAMP',
+  })
+  createAt: Date;
+
+  @UpdateDateColumn({
+    type: 'timestamptz',
+    default: () => 'CURRENT_TIMESTAMP',
+  })
+  updateAt: Date;
 }
